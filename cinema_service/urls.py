bc8ef2c0a6999a517593a7823e6b44ceb0e6cce5
Andrii Chychkan <mywork1728@gmail>
2025-12-30 00:04:12 +0200

    debug toolbar removed from ursl

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 from cinema.urls import router
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
     path("api/cinema/", include((router.urls, "cinema"))),
-] + debug_toolbar_urls()
+]
